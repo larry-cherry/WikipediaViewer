d086c2198c485f6821279deeb200beea7062de37
Larry Cherry <larry.cherry.0406@gmail.com>
2017-05-29 10:57:36 -0500

    experimenting with the wikiapi and searchbar

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("app.js loaded")
+//expands or collapses search bar
 document.getElementById("search-icon").onclick = function(e) {
     console.log("click");
     if (this.classList.contains("collapsed")) {
@@ -9,4 +10,21 @@ document.getElementById("search-icon").onclick = function(e) {
         console.log("it is not collapsed")
         this.classList.add("collapsed")
     }
+}
+//submits wikisearch queriy
+document.getElementById("searchbar").onsubmit = function(e) {
+    e.preventDefault();
+    console.log("form submitted");
+    console.log(this.lastElementChild.value);
+    //example using opensearch query
+    url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=bacon&format=jsonp&callback=?"
+    // debugger;
+    $.ajax({
+    url: url,
+    type: 'GET',
+    cache: false,
+    // data: data,
+  }).done(function(server_data){
+      console.log(server_data);
+  })
 }
